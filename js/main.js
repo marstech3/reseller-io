73c11866953da48fda99c695da37d677ed973090
DhiraNegi <dhiranegi1996@gmail.com>
2021-04-09 08:51:05 +0530

    bulk import and online orders added

--- js/main.js
+++ js/main.js
@@ -6,61 +6,12 @@ $(document).ready(function() {
         $('#bottom-arrow').fadeIn();
         }
     });
-
-    $("#get_connected").mouseenter(
-        function() {
-            $("#inventory_management,#transactions, #store, #dashboard, #orders").hide();
-        });
-    $("#get_connected").mouseleave(
-        function() {
-            $("#inventory_management,#transactions, #store, #dashboard, #orders").show();
-        });
-    $("#inventory_management").mouseenter(
-        function() {
-            $("#get_connected,#transactions, #store, #dashboard, #orders").hide();
-        });
-    $("#inventory_management").mouseleave(
-        function() {
-            $("#get_connected,#transactions, #store, #dashboard, #orders").show();
-        });
-    $("#transactions").mouseenter(
-        function() {
-            $("#get_connected,#inventory_management, #store, #dashboard, #orders").hide();
-        });
-    $("#transactions").mouseleave(
-        function() {
-            $("#get_connected,#inventory_management, #store, #dashboard, #orders").show();
-        });
-    $("#store").mouseenter(
-        function() {
-            $("#get_connected,#transactions, #inventory_management, #dashboard, #orders").hide();
-        });
-    $("#store").mouseleave(
-        function() {
-            $("#get_connected,#transactions, #inventory_management, #dashboard, #orders").show();
-        });
-    $("#dashboard").mouseenter(
-        function() {
-            $("#get_connected,#transactions, #store, #inventory_management, #orders").hide();
-        });
-    $("#dashboard").mouseleave(
-        function() {
-            $("#get_connected,#transactions, #store, #inventory_management, #orders").show();
-        });
-    $("#orders").mouseenter(
-        function() {
-            $("#get_connected,#transactions, #store, #inventory_management, #dashboard").hide();
-        });
-    $("#orders").mouseleave(
-        function() {
-            $("#get_connected,#transactions, #store, #inventory_management, #dashboard").show();
-        });
-
-    function show(divId) {
-        $("#" + divId).show();
-    }
     function showFeatureTour() {
         show('feature_tour');
     }
+    function show(divId) {
+        $("#" + divId).show();
+    }
+    
 });
 
