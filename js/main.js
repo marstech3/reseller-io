36075db56703719c8d42a8ed65fd1b4d4b6b562d
DhiraNegi <dhiranegi1996@gmail.com>
2021-04-18 12:45:01 +0530

    faq edited

--- js/main.js
+++ js/main.js
@@ -42,5 +42,22 @@ $(document).ready(function() {
         $(".div2, .div3, .div4, .div1").hide();
         $(".div5").show();
     });
+    $("#accordion .card .card-link").click(function(){
+        if($(this).find("i.fa").hasClass("fa-minus")){
+
+            $(this).find("i.fa").removeClass("fa-minus");
+            $(this).find("i.fa").addClass("fa-plus");
+        }
+        else if($(this).find("i.fa").hasClass("fa-plus")){
+
+            $(this).find("i.fa").removeClass("fa-plus");
+            $(this).find("i.fa").addClass("fa-minus");
+        }
+
+        $(this).parents(".card").siblings()
+        .find(".card-header .card-link i.fa").removeClass("fa-minus");
+        $(this).parents(".card").siblings()
+        .find(".card-header .card-link i.fa").addClass("fa-plus");
+    })
 });
 
